be9efddbcbbebedc3a42b719f32d5eed9d9def48
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-08-25 16:18:18 +0530

    Update KeyInput.java

--- KeyInput.java
+++ KeyInput.java
@@ -14,11 +14,36 @@ public class KeyInput extends KeyAdapter{
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		
+		for(int i = 0; i<handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			if(tempObject.getId() == ID.Player) {
+				// key events for player 1
+				
+				if(key == KeyEvent.VK_UP)  tempObject.setValY(-5);
+				if(key == KeyEvent.VK_DOWN)  tempObject.setValY(5);
+				if(key == KeyEvent.VK_LEFT)  tempObject.setValX(-5);
+				if(key == KeyEvent.VK_RIGHT)  tempObject.setValX(5);
+				
+			}  
+		}
 		System.out.println(key);
-		
 	}
 	
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
-	}
+		for(int i = 0; i<handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			if(tempObject.getId() == ID.Player) {
+				// key events for player 1
+				
+				if(key == KeyEvent.VK_UP)  tempObject.setValY(0);
+				if(key == KeyEvent.VK_DOWN)  tempObject.setValY(0);
+				if(key == KeyEvent.VK_LEFT)  tempObject.setValX(0);
+				if(key == KeyEvent.VK_RIGHT)  tempObject.setValX(0);
+				
+			}  
+		}
+		
+		if(key == KeyEvent.VK_ESCAPE) System.exit(1);
+	} 
 }
