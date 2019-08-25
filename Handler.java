ec669ad4500f51a0b67b7cf15ee6896605468cdc
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-08-26 00:45:12 +0530

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -9,7 +9,39 @@ public class Handler {
 	
 	public void tick() {
 		for(int i = 0; i<object.size(); i++) {
+			GameObject tempObject = object.get(i);package com.tutorial.main;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+public class Handler {
+	
+	LinkedList<GameObject> object = new LinkedList<GameObject>();
+	
+	public void tick() {
+		for(int i = 0; i<object.size(); i++) {
 			GameObject tempObject = object.get(i);
+			
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for(int i =0; i<object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			tempObject.render(g);
+		}
+	}
+	
+	public void addObject(GameObject object) {
+		this.object.add(object);
+	}
+	
+	public void removeObject(GameObject object) {
+		this.object.remove(object);
+	}
+}
+
 			
 			tempObject.tick();
 		}
