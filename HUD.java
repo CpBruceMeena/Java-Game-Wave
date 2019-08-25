b9b49b1046f8bfc516e77996e2884c261ef46885
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-08-26 00:43:47 +0530

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -8,20 +8,19 @@ public class HUD {
 	public static int HEALTH = 100;
 	
 	public void tick() {
-		HEALTH--;
 		
 		HEALTH = Game.clamp(HEALTH, 0, 100);
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.gray);
-		g.fillRect(35, 35, 200,  32);
+		g.fillRect(15, 15, 200,  32);
 		
 
 		g.setColor(Color.green);
-		g.fillRect(35, 35, HEALTH*2,  32);
-
+		g.fillRect(15, 15, HEALTH*2,  32);
+		
 		g.setColor(Color.white);
-		g.drawRect(35, 35, 200,  32);
+		g.drawRect(15, 15, 200,  32);
 	}
 }
