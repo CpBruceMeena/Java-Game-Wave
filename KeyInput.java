f17b969f7a40b3d73d10a5677b0f77b0ca382ba0
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-08-28 00:56:04 +0530

    Update KeyInput.java

--- KeyInput.java
+++ KeyInput.java
@@ -7,8 +7,15 @@ public class KeyInput extends KeyAdapter{
 	
 	public Handler handler;
 	
+	private boolean[] keyDown = new boolean[4];
+	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		
+		keyDown[0] = false;
+		keyDown[1] = false;
+		keyDown[2] = false;
+		keyDown[3] = false;
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -19,14 +26,14 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Player) {
 				// key events for player 1
 				
-				if(key == KeyEvent.VK_UP)  tempObject.setValY(-5);
-				if(key == KeyEvent.VK_DOWN)  tempObject.setValY(5);
-				if(key == KeyEvent.VK_LEFT)  tempObject.setValX(-5);
-				if(key == KeyEvent.VK_RIGHT)  tempObject.setValX(5);
+				if(key == KeyEvent.VK_UP)	 {  tempObject.setValY(-5); keyDown[0] = true;} 				
+				if(key == KeyEvent.VK_DOWN)  {  tempObject.setValY(5);  keyDown[1] = true;}
+				if(key == KeyEvent.VK_LEFT)  {  tempObject.setValX(-5); keyDown[2] = true;}
+				if(key == KeyEvent.VK_RIGHT) {  tempObject.setValX(5);  keyDown[3] = true;}
 				
 			}  
-		}
-		System.out.println(key);
+		}	
+		if(key == KeyEvent.VK_ESCAPE) System.exit(1);
 	}
 	
 	public void keyReleased(KeyEvent e) {
@@ -36,14 +43,19 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getId() == ID.Player) {
 				// key events for player 1
 				
-				if(key == KeyEvent.VK_UP)  tempObject.setValY(0);
-				if(key == KeyEvent.VK_DOWN)  tempObject.setValY(0);
-				if(key == KeyEvent.VK_LEFT)  tempObject.setValX(0);
-				if(key == KeyEvent.VK_RIGHT)  tempObject.setValX(0);
+				if(key == KeyEvent.VK_UP) keyDown[0] = false;//tempObject.setValY(0);
+				if(key == KeyEvent.VK_DOWN)keyDown[1] = false; // tempObject.setValY(0);
+				if(key == KeyEvent.VK_LEFT)  keyDown[2]  = false;//tempObject.setValX(0);
+				if(key == KeyEvent.VK_RIGHT)  keyDown[3] = false;//tempObject.setValX(0);
 				
+				
+				//vertical movement 
+				if(!keyDown[0] && !keyDown[1])	 tempObject.setValY(0);
+
+				if(!keyDown[2] && !keyDown[3])	 tempObject.setValX(0);
 			}  
 		}
-		
+		 
 		if(key == KeyEvent.VK_ESCAPE) System.exit(1);
 	} 
 }
