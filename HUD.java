f6e83b3aea6020be0b51888fe9c47c067eff70c9
CpBruceMeena <39619151+CpBruceMeena@users.noreply.github.com>
2019-09-04 12:02:24 +0530

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -1,21 +1,19 @@
 package com.tutorial.main;
-
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class HUD {
 	
-	public static int HEALTH = 100;
+	public static float HEALTH = 100;
 	
-	private int greenValue = 255;
+	private float greenValue = 255;
 	private int score = 0;
 	private int level = 1;
 	
 	public void tick() {
 		
-		HEALTH = Game.clamp(HEALTH, 0, 100);
-		
-		greenValue = Game.clamp(greenValue, 0, 255);
+		HEALTH = Game.clamp((int)HEALTH, 0, 100);
+		greenValue = Game.clamp((int)greenValue, 0, 255);
 		greenValue = HEALTH*2;
 		score++;
 	}
@@ -24,8 +22,8 @@ public class HUD {
 		g.setColor(Color.gray);
 		g.fillRect(15, 15, 200,  32);
 		
-		g.setColor(new Color(75, greenValue, 0));
-		g.fillRect(15, 15, HEALTH*2,  32);
+		g.setColor(new Color(75, (int)greenValue, 0));
+		g.fillRect(15, 15, (int)HEALTH*2,  32);
 		
 		g.setColor(Color.white);
 		g.drawRect(15, 15, 200,  32);
@@ -34,13 +32,15 @@ public class HUD {
 		g.drawString("Level: " + level, 15 , 80 );
 	}
 	
-	public void score(int score) {
-		this.score = score;
-	}
-	
+
 	public int getscore() {
 		return score; 
 	}
+	
+	public void setscore(int score) {
+		this.score = score;
+	}
+	
 	
 	public int getLevel() {
 		return level;
@@ -50,5 +50,3 @@ public class HUD {
 		this.level = level;
 	}
 }
-
-
